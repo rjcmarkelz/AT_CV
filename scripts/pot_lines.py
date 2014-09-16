c50ae932f4733538e11bc97ce7e72a310b24b83c
Cody Markelz <rjmarkelz@ucdavis.edu>
2014-09-15 19:31:37 -0700

    adjust parameters for line finder

--- scripts/pot_lines.py
+++ scripts/pot_lines.py
@@ -44,8 +44,8 @@ for rho,theta in lines[0]:
     cv2.line(input_copy1,(x1,y1),(x2,y2),(0,0,255),2)
 cv2.imshow("input_copy1",input_copy1)
 
-minLineLength = 2000
-maxLineGap = 100
+minLineLength = 4000
+maxLineGap = 50
 lines = cv2.HoughLinesP(dilation,1,np.pi/180,100,minLineLength,maxLineGap)
 for x1,y1,x2,y2 in lines[0]:
     cv2.line(input_copy2,(x1,y1),(x2,y2),(0,255,0),2)
