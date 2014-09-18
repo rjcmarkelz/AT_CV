4d3ecd412c4d25e6c31af0c219ea029cea4b8f63
Cody Markelz <rjmarkelz@ucdavis.edu>
2014-09-18 11:07:22 -0700

    pot detection on 3D image

--- scripts/pot_lines.py
+++ scripts/pot_lines.py
@@ -13,26 +13,38 @@ input_image = cv2.imread(args["image"])
 
 hsb_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
 H_channel,S_channel,hsB_channel = cv2.split(hsb_image)
-cv2.imshow("S",S_channel)
+# cv2.imshow("S",S_channel)
 cv2.imshow("H",H_channel)
-cv2.imshow("hsB",hsB_channel)
+# cv2.imshow("hsB",hsB_channel)
 
-hist = cv2.calcHist([S_channel], [0], None, [256], [0, 256])
-# Plot the histogram
-plt.figure()
-plt.title("S Histogram")
-plt.xlabel("Bins")
-plt.ylabel("# of Pixels")
-plt.plot(hist)
-plt.xlim([0, 256])
-plt.show()
-cv2.waitKey(0)
+# hist = cv2.calcHist([H_channel], [0], None, [256], [0, 256])
+# # Plot the histogram
+# plt.figure()
+# plt.title("S Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("# of Pixels")
+# plt.plot(hist)
+# plt.xlim([0, 256])
+# plt.show()
+# cv2.waitKey(0)
 
-blur = cv2.bilateralFilter(S_channel,9,100,100)
+blur = cv2.bilateralFilter(H_channel,9,100,100)
 cv2.imshow("blur", blur)
 
-(T, thresh_image) = cv2.threshold(blur, 100, 120, cv2.THRESH_BINARY)
+# hist = cv2.calcHist([blur], [0], None, [256], [0, 256])
+# # Plot the histogram
+# plt.figure()
+# plt.title("blur Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("# of Pixels")
+# plt.plot(hist)
+# plt.xlim([0, 256])
+# plt.show()
+# cv2.waitKey(0)
+
+(T, thresh_image) = cv2.threshold(blur, 75, 110, cv2.THRESH_BINARY)
 cv2.imshow("Threshold Binary", thresh_image)
+
 canny_image = cv2.Canny(thresh_image, 100, 200, apertureSize = 3)
 cv2.imshow("Canny", canny_image)
 
@@ -40,32 +52,32 @@ kernel = np.ones((5,5),np.uint8)
 dilation = cv2.dilate(canny_image,kernel,iterations = 2)
 cv2.imshow("Canny dilation", dilation)
 
-lines = cv2.HoughLines(dilation,1,np.pi/180, 150)
+# lines = cv2.HoughLines(dilation,1,np.pi/180, 150)
 
-# print lines
+# # print lines
 input_copy1 = input_image.copy()
 input_copy2 = input_image.copy()
 
-# for rho,theta in lines[0]:
-#     a = np.cos(theta)
-#     b = np.sin(theta)
-#     x0 = a*rho
-#     y0 = b*rho
-#     x1 = int(x0 + 1000*(-b))
-#     y1 = int(y0 + 1000*(a))
-#     x2 = int(x0 - 1000*(-b))
-#     y2 = int(y0 - 1000*(a))
-#     cv2.line(input_copy1,(x1,y1),(x2,y2),(0,0,255),2)
-    # print a, b, x1, y1, x2, y2
-#cv2.imshow("input_copy1",input_copy1)
+# # for rho,theta in lines[0]:
+# #     a = np.cos(theta)
+# #     b = np.sin(theta)
+# #     x0 = a*rho
+# #     y0 = b*rho
+# #     x1 = int(x0 + 1000*(-b))
+# #     y1 = int(y0 + 1000*(a))
+# #     x2 = int(x0 - 1000*(-b))
+# #     y2 = int(y0 - 1000*(a))
+# #     cv2.line(input_copy1,(x1,y1),(x2,y2),(0,0,255),2)
+#     # print a, b, x1, y1, x2, y2
+# #cv2.imshow("input_copy1",input_copy1)
 
 minLineLength = 10000
-maxLineGap = 100
+maxLineGap = 40
 linesP = cv2.HoughLinesP(dilation,1,np.pi/180,100,minLineLength,maxLineGap)
-print lines
+# print lines
 for x1,y1,x2,y2 in linesP[0]:
     cv2.line(input_copy2,(x1,y1),(x2,y2),(0,255,0),2)
-cv2.line(input_copy2,(5,70),(11,384),(0,0,255),2)
+#cv2.line(input_copy2,(5,70),(11,384),(0,0,255),2)
 cv2.imshow("input_copy2",input_copy2)
 
 # cv2.imwrite('houghlines.jpg',in)
