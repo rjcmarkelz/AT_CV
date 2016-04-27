cb04e1e6f61c24478d765600ebd529c5f6eec404
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-04-26 22:17:35 -0700

    path directory

--- scripts/arabidopsis_plants_iview.py
+++ scripts/arabidopsis_plants_iview.py
@@ -1,112 +1,54 @@
 #########
 # Cody Markelz
-# September 18, 2015
+# April 26, 2016
 # markelz@gmail.com
 # github, twitter, bitbucket: rjcmarkelz
-#
-
+#########
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import argparse
+import os
+
+root_dir = '.'
+
+for files in os.walk(root_dir):
+    for file in files:
+        print file
 
 # get some info from the user
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image")
+    help = "Path to the image")
 args = vars(ap.parse_args())
 # Load the image and display it
+
+# files_in_dir = os.listdir(inputdir)
+# for file_in_dir in files_in_dir:
+
 input_image = cv2.imread(args["image"])
 
-#convert image to HSV colorspace for pot detection
 hsb_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
 H_channel,S_channel,hsB_channel = cv2.split(hsb_image)
+lab_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2LAB)
+l_channel,a_channel,b_channel = cv2.split(lab_image)
+(aT, a_thresh_image) = cv2.threshold(a_channel, 110, 130, cv2.THRESH_BINARY)
+cv2.imshow("a_channel Threshold Binary", a_thresh_image)
+cv2.imwrite('output.tif', a_thresh_image)
+
+dst = cv2.inRange(a_thresh_image, 110, 130)
+no_black = cv2.countNonZero(dst)
+print('The number of black pixels is: ' + str(no_black))
 # cv2.imshow("H",H_channel)
 # cv2.imshow("S",S_channel)
 # cv2.imshow("hsB",hsB_channel)
 
 # Convert original image to the L*a*b* color space for arabidopsis detection
-lab_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2LAB)
-l_channel,a_channel,b_channel = cv2.split(lab_image)
+
 #cv2.imshow("L",l_channel)
-# cv2.imshow("a",a_channel) # focus on a channel because it provides best contrast for arabidopsis
+cv2.imshow("a",a_channel) # focus on a channel because it provides best contrast for arabidopsis
 #cv2.imshow("b",b_channel)
 
-
-
-#########
-#########
-#########
-# Pot identification
-#########
-#########
-#########
-
-# take a look at the H channel histogram for pot detection, uncomment when finding correct threshold
-# hist = cv2.calcHist([H_channel], [0], None, [256], [0, 256])
-
-# Plot the histogram
-# plt.figure()
-# plt.title("H Histogram")
-# plt.xlabel("Bins")
-# plt.ylabel("# of Pixels")
-# plt.plot(hist)
-# plt.xlim([0, 256])
-# plt.show()
-# cv2.waitKey(0)
-
-# blur image to get better edges
-blur = cv2.bilateralFilter(H_channel, 9, 100, 100)
-cv2.imshow("blur", blur)
-
-# hist = cv2.calcHist([blur], [0], None, [256], [0, 256])
-# # Plot the histogram
-# plt.figure()
-# plt.title("blur Histogram")
-# plt.xlabel("Bins")
-# plt.ylabel("# of Pixels")
-# plt.plot(hist)
-# plt.xlim([0, 256])
-# plt.show()
-# cv2.waitKey(0)
-
-# pick peaks to threshold around
-# (T, thresh_image) = cv2.threshold(blur, 75, 110, cv2.THRESH_BINARY)
-# # cv2.imshow("Threshold Binary", thresh_image)
-
-# # canny edge detection on the thresholded image
-# canny_image = cv2.Canny(thresh_image, 100, 200, apertureSize = 3)
-# # cv2.imshow("Canny", canny_image)
-
-# # set kernal to "Dilate" the lines from the canny image
-# # this step is necessary to bring out the edges of the pots
-# # play with the size of the array and the number of iterations for various results
-# kernel = np.ones((5,5), np.uint8)
-# dilation = cv2.dilate(canny_image, kernel, iterations = 2)
-# cv2.imshow("Canny dilation", dilation)
-
-# # # print lines
-# input_copy2 = input_image.copy()
-
-# # set parameters for HoughLinesP 
-# # also play with these to get more or less numbers of lines
-# minLineLength = 10000
-# maxLineGap = 40
-# linesP = cv2.HoughLinesP(dilation, 1, np.pi/180, 100, minLineLength, maxLineGap)
-
-# # extract endpoints and draw HoughLines
-# for x1,y1,x2,y2 in linesP[0]:
-#     cv2.line(input_copy2,(x1,y1),(x2,y2), (0,255,0) ,2)
-
-# # print the lines and pick the end points of one of the lines to display in red
-# # this helps to see how the lines are being found, uncomment next two lines to see
-# # print lines
-# #cv2.line(input_copy2,(5,70),(11,384),(0,0,255),2)
-# cv2.imshow("input_copy2",input_copy2)
-
-# write output to file
-# cv2.imwrite('houghlines.jpg',in)
-# cv2.imwrite('houghlinesP.jpg',input_copy2)
 
 #########
 #########
