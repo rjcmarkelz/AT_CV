e74bb5e2de5808bd2bdcbee6b6e2f8ecf8d1ac6a
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-04-26 21:39:16 -0700

    count pixels

--- scripts/arabidopsis_plants_iview.py
+++ scripts/arabidopsis_plants_iview.py
@@ -1,13 +1,9 @@
 #########
 # Cody Markelz
-# September 18, 2014
+# September 18, 2015
 # markelz@gmail.com
 # github, twitter, bitbucket: rjcmarkelz
-<<<<<<< HEAD
-=======
-# GPL license- Keep it open. 
->>>>>>> develop
-#########
+#
 
 import cv2
 import numpy as np
@@ -75,38 +71,38 @@ cv2.imshow("blur", blur)
 # cv2.waitKey(0)
 
 # pick peaks to threshold around
-(T, thresh_image) = cv2.threshold(blur, 75, 110, cv2.THRESH_BINARY)
-# cv2.imshow("Threshold Binary", thresh_image)
+# (T, thresh_image) = cv2.threshold(blur, 75, 110, cv2.THRESH_BINARY)
+# # cv2.imshow("Threshold Binary", thresh_image)
 
-# canny edge detection on the thresholded image
-canny_image = cv2.Canny(thresh_image, 100, 200, apertureSize = 3)
-# cv2.imshow("Canny", canny_image)
+# # canny edge detection on the thresholded image
+# canny_image = cv2.Canny(thresh_image, 100, 200, apertureSize = 3)
+# # cv2.imshow("Canny", canny_image)
 
-# set kernal to "Dilate" the lines from the canny image
-# this step is necessary to bring out the edges of the pots
-# play with the size of the array and the number of iterations for various results
-kernel = np.ones((5,5), np.uint8)
-dilation = cv2.dilate(canny_image, kernel, iterations = 2)
-cv2.imshow("Canny dilation", dilation)
+# # set kernal to "Dilate" the lines from the canny image
+# # this step is necessary to bring out the edges of the pots
+# # play with the size of the array and the number of iterations for various results
+# kernel = np.ones((5,5), np.uint8)
+# dilation = cv2.dilate(canny_image, kernel, iterations = 2)
+# cv2.imshow("Canny dilation", dilation)
 
+# # # print lines
+# input_copy2 = input_image.copy()
+
+# # set parameters for HoughLinesP 
+# # also play with these to get more or less numbers of lines
+# minLineLength = 10000
+# maxLineGap = 40
+# linesP = cv2.HoughLinesP(dilation, 1, np.pi/180, 100, minLineLength, maxLineGap)
+
+# # extract endpoints and draw HoughLines
+# for x1,y1,x2,y2 in linesP[0]:
+#     cv2.line(input_copy2,(x1,y1),(x2,y2), (0,255,0) ,2)
+
+# # print the lines and pick the end points of one of the lines to display in red
+# # this helps to see how the lines are being found, uncomment next two lines to see
 # # print lines
-input_copy2 = input_image.copy()
-
-# set parameters for HoughLinesP 
-# also play with these to get more or less numbers of lines
-minLineLength = 10000
-maxLineGap = 40
-linesP = cv2.HoughLinesP(dilation, 1, np.pi/180, 100, minLineLength, maxLineGap)
-
-# extract endpoints and draw HoughLines
-for x1,y1,x2,y2 in linesP[0]:
-    cv2.line(input_copy2,(x1,y1),(x2,y2), (0,255,0) ,2)
-
-# print the lines and pick the end points of one of the lines to display in red
-# this helps to see how the lines are being found, uncomment next two lines to see
-# print lines
-#cv2.line(input_copy2,(5,70),(11,384),(0,0,255),2)
-cv2.imshow("input_copy2",input_copy2)
+# #cv2.line(input_copy2,(5,70),(11,384),(0,0,255),2)
+# cv2.imshow("input_copy2",input_copy2)
 
 # write output to file
 # cv2.imwrite('houghlines.jpg',in)
@@ -136,6 +132,10 @@ cv2.imshow("input_copy2",input_copy2)
 (aT, a_thresh_image) = cv2.threshold(a_channel, 110, 130, cv2.THRESH_BINARY)
 cv2.imshow("a_channel Threshold Binary", a_thresh_image)
 cv2.imwrite('output.tif', a_thresh_image)
+
+dst = cv2.inRange(a_thresh_image, 110, 130)
+no_black = cv2.countNonZero(dst)
+print('The number of black pixels is: ' + str(no_black))
 
 #a_thresh_image2 = a_thresh_image.copy()
 # cv2.imshow("a_thresh_2",a_thresh_image2)
